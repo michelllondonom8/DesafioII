47e71238902fe3c8a3b3de879125a42ba4792595
michelllondonom8 <michell.londonom@udea.edu.co>
2025-10-18 22:15:18 -0500

    Creacion de clase Colaborador

--- Desafio/colaborador.cpp
+++ Desafio/colaborador.cpp
@@ -1,3 +1,32 @@
 #include "colaborador.h"
+#include <iostream>
+using namespace std;
 
-Colaborador::Colaborador() {}
+Colaborador::Colaborador() {
+    nombre = "";
+    apellido = "";
+    codigoAfiliacion = "";
+    tipo = "";
+}
+
+
+Colaborador::Colaborador(const string& nom, const string& ape, const string& cod, const string& tipo) {
+    this->nombre = nom;
+    this->apellido = ape;
+    this->codigoAfiliacion = cod;
+    this->tipo = tipo;
+}
+
+
+Colaborador::~Colaborador() {}
+
+void Colaborador::mostrar() const {
+    cout << "Colaborador: " << nombre << " " << apellido
+         << " | Código: " << codigoAfiliacion
+         << " | Tipo: " << tipo << endl;
+}
+
+// Devuelve el tipo de colaborador
+string Colaborador::getTipo() const {
+    return tipo;
+}
--- Desafio/colaborador.h
+++ Desafio/colaborador.h
@@ -1,10 +1,28 @@
 #ifndef COLABORADOR_H
 #define COLABORADOR_H
+#include <string>
+#include <iostream>
+using namespace std;
 
-class Colaborador
-{
+class Colaborador {
+private:
+    string nombre;
+    string apellido;
+    string codigoAfiliacion;
+    string tipo;
+
 public:
+
     Colaborador();
+    Colaborador(const string& nom, const string& ape, const string& cod, const string& tipo);
+
+    ~Colaborador();
+
+
+    void mostrar() const;
+
+
+    string getTipo() const;
 };
 
 #endif // COLABORADOR_H
