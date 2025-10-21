85cf043de1600e05ab09387d587b8d41cb507ba5
michelllondonom8 <michell.londonom@udea.edu.co>
2025-10-20 19:30:59 -0500

    Creacion de constructor, getters y defino atributos iniciales de la clase ListaFavoritos

--- Desafio/listafavoritos.cpp
+++ Desafio/listafavoritos.cpp
@@ -1,3 +1,84 @@
 #include "listafavoritos.h"
+#include <iostream>
 
-ListaFavoritos::ListaFavoritos() {}
+using namespace std;
+
+static const int MAX_CAP = 10000;
+
+
+int ListaFavoritos::indiceDeCancion(int id) const {
+    for (int i = 0; i < cantidad; ++i) {
+        if (canciones[i] == id) return i;
+    }
+    return -1;
+}
+
+
+ListaFavoritos::ListaFavoritos(const string& propietario, const string& nombre)
+    : propietarioNick(propietario), nombreLista(nombre),
+    capacidad(MAX_CAP), cantidad(0),
+    agregadasPorSeguir(nullptr), cantidadAgregadasPorSeguir(0),
+    siguiendoA(""), estaSiguiendo(false)
+{
+    canciones = new int[capacidad];
+    // inicializar (opcional)
+    for (int i = 0; i < capacidad; ++i) canciones[i] = 0;
+    // seed para rand (si no lo hizo main)
+    srand((unsigned) time(nullptr));
+}
+
+ListaFavoritos::ListaFavoritos(const ListaFavoritos& otro)
+    : propietarioNick(otro.propietarioNick), nombreLista(otro.nombreLista),
+    capacidad(otro.capacidad), cantidad(otro.cantidad),
+    agregadasPorSeguir(nullptr), cantidadAgregadasPorSeguir(0),
+    siguiendoA(otro.siguiendoA), estaSiguiendo(otro.estaSiguiendo)
+{
+    canciones = new int[capacidad];
+    for (int i = 0; i < cantidad; ++i) canciones[i] = otro.canciones[i];
+
+    if (otro.cantidadAgregadasPorSeguir > 0) {
+        cantidadAgregadasPorSeguir = otro.cantidadAgregadasPorSeguir;
+        agregadasPorSeguir = new int[cantidadAgregadasPorSeguir];
+        for (int i = 0; i < cantidadAgregadasPorSeguir; ++i)
+            agregadasPorSeguir[i] = otro.agregadasPorSeguir[i];
+    } else {
+        agregadasPorSeguir = nullptr;
+    }
+}
+
+ListaFavoritos& ListaFavoritos::operator=(const ListaFavoritos& otro) {
+    if (this == &otro) return *this;
+    delete[] canciones;
+    delete[] agregadasPorSeguir;
+
+    propietarioNick = otro.propietarioNick;
+    nombreLista = otro.nombreLista;
+    capacidad = otro.capacidad;
+    cantidad = otro.cantidad;
+
+    canciones = new int[capacidad];
+    for (int i = 0; i < cantidad; ++i) canciones[i] = otro.canciones[i];
+
+    if (otro.cantidadAgregadasPorSeguir > 0) {
+        cantidadAgregadasPorSeguir = otro.cantidadAgregadasPorSeguir;
+        agregadasPorSeguir = new int[cantidadAgregadasPorSeguir];
+        for (int i = 0; i < cantidadAgregadasPorSeguir; ++i)
+            agregadasPorSeguir[i] = otro.agregadasPorSeguir[i];
+    } else {
+        agregadasPorSeguir = nullptr;
+        cantidadAgregadasPorSeguir = 0;
+    }
+
+    siguiendoA = otro.siguiendoA;
+    estaSiguiendo = otro.estaSiguiendo;
+
+    return *this;
+}
+
+string ListaFavoritos::getPropietario() const { return propietarioNick; }
+int ListaFavoritos::getCantidad() const { return cantidad; }
+int ListaFavoritos::getCancionEn(int indice) const {
+    if (indice < 0 || indice >= cantidad) return -1;
+    return canciones[indice];
+}
+
--- Desafio/listafavoritos.h
+++ Desafio/listafavoritos.h
@@ -1,10 +1,48 @@
 #ifndef LISTAFAVORITOS_H
 #define LISTAFAVORITOS_H
 
-class ListaFavoritos
-{
+#include <string>
+using namespace std;
+
+class ListaFavoritos {
+private:
+    string propietarioNick;
+    string nombreLista;
+
+    int *canciones;
+    int capacidad;
+    int cantidad;
+
+    int *agregadasPorSeguir;
+    int cantidadAgregadasPorSeguir;
+    string siguiendoA;
+    bool estaSiguiendo;
+
+    int indiceDeCancion(int id) const;
+
 public:
-    ListaFavoritos();
+
+    ListaFavoritos(const string& propietario, const string& nombre = "Favoritos");
+    ListaFavoritos(const ListaFavoritos& otro);
+    ListaFavoritos& operator=(const ListaFavoritos& otro);
+    ~ListaFavoritos();
+
+    string getPropietario() const;
+    int getCantidad() const;
+    int getCancionEn(int indice) const;
+
+    bool agregar(int idCancion);
+    bool eliminar(int idCancion);
+    void mezclar();
+    void ejecutarLista();
+    void ejecutarLista(bool aleatorio);
+
+    bool seguirOtraLista(ListaFavoritos* otra);
+    bool dejarDeSeguir();
+
+    int operator[](int indice) const;
+    bool operator==(const ListaFavoritos& otro) const;
 };
+
 
 #endif // LISTAFAVORITOS_H
