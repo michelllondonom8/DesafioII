bf9f1f3665a04278e62da6bef48b21b4aad0457d
carlos mario rendon martinez <carlos.rendonm1@udea.edu.co>
2025-10-23 11:33:30 -0500

    Se sube la implementación de artista.cpp y artista.h

--- Desafio/artista.h
+++ Desafio/artista.h
@@ -1,10 +1,33 @@
 #ifndef ARTISTA_H
 #define ARTISTA_H
 
-class Artista
-{
+#include <iostream>
+#include <string>
+#include "album.h"
+using namespace std;
+
+class Artista {
+private:
+    string nombre;
+    string pais;
+
+    Album** albumes;
+    int totalAlbumes;
+    int capacidad;
+
+    void ampliarEspacio();
+
 public:
     Artista();
+    Artista(const string& _nombre, const string& _pais);
+    ~Artista();
+
+    bool agregarAlbum(Album* a);
+    int contarCanciones() const;
+    void mostrarArtista() const;
+
+    string getNombre() const;
+    int getTotalAlbumes() const;
 };
 
-#endif // ARTISTA_H
+#endif
